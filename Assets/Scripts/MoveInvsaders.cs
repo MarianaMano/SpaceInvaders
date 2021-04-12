b9ecb7cb14c62151a6371adee4c277d72f41d9da
mariana mano <40200338@esmad.ipp.pt>
2021-04-12 14:29:23 +0100

    Create MoveInvsaders.cs

---
+++ Assets/Scripts/MoveInvsaders.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInvsaders : MonoBehaviour
+{
+
+    [SerializeField]
+    float velocity = 0.1f;
+    [SerializeField]
+    float minX = -0.7f;
+    [SerializeField]
+    float maxX = 0.7f;
+
+    [SerializeField]
+    float moveTime = 1f;
+
+    Vector3 direction = Vector3.right; //(1,0,0)
+    float tempoQuePassou = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempoQuePassou += Time.deltaTime;
+        if(tempoQuePassou >= moveTime)
+        {
+            transform.position += velocity * direction;
+            tempoQuePassou = 0f;
+            if(transform.position.z <= minX || transform.position.x >= maxX)
+            {
+                direction *= -1f;
+                transform.position += velocity * Vector3.down;
+            }
+        }
+    }
+}
